91a86cd0514de40550f4288308b17f81def796ea
Yash Jindal <67179148+yashjnx@users.noreply.github.com>
2020-11-21 15:12:28 +0530

    Create Assignment_2_Q2.c

---
+++ Assignment_2_Q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){    
+    for(int i=1;i<=100;i++)
+    {   
+        // if number module i is equal to 0, then number is even
+        if(i%2==0)
+        {
+            printf("%d\n", i);    
+        }
+    }
+    return 0;
+}
